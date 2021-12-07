5b08bcdb14683d35975fd8e1f47a2ce72c76826d
keegran <78465252+keegran191@users.noreply.github.com>
2021-12-07 18:59:28 +0700

    borh

--- Project/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Project/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,7 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                openOomHome();
+            }
+            public void openOomHome(){
+                Intent intent = new Intent(MainActivity.this,OomHouse.class);
+                startActivity(intent);
+            }
+
+        });
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setItemIconTintList(null);
         // Passing each menu ID as a set of Ids because each
--- Project/MyApplication/app/src/main/java/com/example/myapplication/OomHouse.java
+++ Project/MyApplication/app/src/main/java/com/example/myapplication/OomHouse.java
@@ -2,13 +2,47 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class OomHouse extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oom_house);
+
+        button = (Button) findViewById(R.id.buttonBack);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                openOomHome();
+            }
+            public void openOomHome(){
+                Intent intent = new Intent(OomHouse.this,MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        ImageView imageView2 = (ImageView) findViewById(R.id.imageView5);
+        imageView2.setImageResource(R.drawable.unnamed);
+        imageView2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(), "ห้องที่พัก",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public void toastImg4 (View v){
+        Toast.makeText(OomHouse.this,"Hey",Toast.LENGTH_SHORT);
     }
 }
